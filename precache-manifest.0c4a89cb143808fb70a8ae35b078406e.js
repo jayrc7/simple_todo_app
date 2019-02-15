6a5a77fb17525acf4cc8a340df474dd895e4c90b
Jason Cabrera <cabrerajason7@gmail.com>
2019-02-15 14:43:18 -0800

    Updates

---
+++ precache-manifest.0c4a89cb143808fb70a8ae35b078406e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simple_todo_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6309473ba912f400d5cc",
+    "url": "/simple_todo_app/static/js/runtime~main.6309473b.js"
+  },
+  {
+    "revision": "d18a9bb0b3232635978b",
+    "url": "/simple_todo_app/static/js/main.d18a9bb0.chunk.js"
+  },
+  {
+    "revision": "a75237ea0ef595a14061",
+    "url": "/simple_todo_app/static/js/2.a75237ea.chunk.js"
+  },
+  {
+    "revision": "d18a9bb0b3232635978b",
+    "url": "/simple_todo_app/static/css/main.8032568b.chunk.css"
+  },
+  {
+    "revision": "1a14c343949ec78769103f1f8c50468f",
+    "url": "/simple_todo_app/index.html"
+  }
+];
